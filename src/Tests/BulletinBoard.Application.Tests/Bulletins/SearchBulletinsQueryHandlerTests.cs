a10f42b22debb756d1eb75588e6e73e411abccfa
Константин <micrantha@yandex.ru>
2024-06-17 14:06:33 +0300

    Revert "Удалены тесты"
    This reverts commit fa5948b5939bae127125b1bff2936ef8717326cc.

---
+++ src/Tests/BulletinBoard.Application.Tests/Bulletins/SearchBulletinsQueryHandlerTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoFixture;
+using BulletinBoard.Application.Bulletins.SearchBulletins;
+using BulletinBoard.Application.Models.Bulletins;
+using BulletinBoard.Application.Repositories;
+using BulletinBoard.Domain.Entities;
+using BulletinBoard.Domain.Tests.Tools;
+using FluentAssertions;
+using Moq;
+
+namespace BulletinBoard.Application.Tests.Bulletins;
+
+public class SearchBulletinsQueryHandlerTests
+{
+    private readonly IFixture _fixture = FixtureExtensions.GetFixtureWithAllCustomizations();
+
+    [Fact]
+    public async Task Handle_ValidQuery_ReturnsIEnumerableWithSingleUser()
+    {
+        // Arrange
+        var searchFilters = _fixture.Create<BulletinsSearchFilters>();
+        var bulletin = _fixture.Create<Bulletin>();
+
+        var bulletinRepositoryMock = new Mock<IBulletinRepository>(MockBehavior.Strict);
+        bulletinRepositoryMock
+            .Setup(r => r.SearchAsync(
+                It.Is<BulletinsSearchFilters>(f => f.Page == searchFilters.Page &&
+                                                   f.Count == searchFilters.Count &&
+                                                   f.SearchNumber == searchFilters.SearchNumber &&
+                                                   f.SearchUserId == searchFilters.SearchUserId &&
+                                                   f.SortBy == searchFilters.SortBy &&
+                                                   f.Desc == searchFilters.Desc &&
+                                                   f.RatingFrom == searchFilters.RatingFrom &&
+                                                   f.RatingTo == searchFilters.RatingTo &&
+                                                   f.CreatedFromUtc == searchFilters.CreatedFromUtc &&
+                                                   f.CreatedToUtc == searchFilters.CreatedToUtc &&
+                                                   f.ExpiryFromUtc == searchFilters.ExpiryFromUtc &&
+                                                   f.ExpiryToUtc == searchFilters.ExpiryToUtc),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new[] { bulletin });
+
+        var tenantMock = new Mock<ITenant>(MockBehavior.Strict);
+        tenantMock
+            .SetupGet(t => t.Bulletins)
+            .Returns(bulletinRepositoryMock.Object);
+
+        var tenantFactoryMock = new Mock<ITenantFactory>(MockBehavior.Strict);
+        tenantFactoryMock
+            .Setup(f => f.GetTenant())
+            .Returns(tenantMock.Object);
+
+        var query = new SearchBulletinsQuery(searchFilters);
+        var handler = new SearchBulletinsQueryHandler(tenantFactoryMock.Object);
+
+        // Act
+        var bulletins = await handler.Handle(query);
+
+        // Assert
+        bulletinRepositoryMock.VerifyAll();
+        tenantMock.VerifyAll();
+        tenantFactoryMock.VerifyAll();
+
+        bulletins.Should().ContainSingle(b => b == bulletin);
+    }
+
+    [Fact]
+    public async Task Handle_RequestIsNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        var tenantFactoryMock = new Mock<ITenantFactory>();
+        SearchBulletinsQuery request = null!;
+        var handler = new SearchBulletinsQueryHandler(tenantFactoryMock.Object);
+
+        // Act
+        var action = async () => await handler.Handle(request);
+
+        // Assert
+        await action.Should()
+            .ThrowAsync<ArgumentNullException>()
+            .WithParameterName(nameof(request));
+    }
+}
---
+++ src/Tests/BulletinBoard.Domain.Tests/Tools/BulletinCustomization.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using BulletinBoard.Domain.Entities;
+
+namespace BulletinBoard.Domain.Tests.Tools;
+
+public class BulletinCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        var id = fixture.Create<Guid>();
+        var number = fixture.Create<int>();
+        var text = fixture.Create<string>();
+        var rating = fixture.Create<int>();
+        var createdUtc = fixture.Create<DateTime>();
+        var expiryUtc = createdUtc.AddDays(1);
+        var userId = fixture.Create<Guid>();
+
+        fixture.Customize<Bulletin>(composer =>
+            composer.FromFactory(() =>
+                new Bulletin(id, number, text, rating, createdUtc, expiryUtc, userId)));
+    }
+}
